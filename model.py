c5a4686dbe7e6586e168dc29734e597f8363da26
Okrio <jingbiao_huang@163.com>
2023-10-13 00:15:50 +0800

    update alignblock

--- model.py
+++ model.py
@@ -4,6 +4,56 @@ import torch
 import torch.nn as nn
 import numpy as np
 
+
+class AlignBlock(nn.Module):
+    def __init__(self,in_ch=128,h=32, max_delay_block=32) -> None:
+        super().__init__()
+        # self.p_dim = pdim
+        self.h = h
+        self.max_delay = max_delay_block
+        self.pointwise_conv1 = nn.Conv2d(in_ch,h, kernel_size=(1,1) )
+        self.pointwise_conv2 = nn.Conv2d(in_ch, h, kernel_size=(1,1))
+        self.conv2d = nn.Conv2d(h, 1, kernel_size=(5,3),stride=(1,1),padding=(3,1))
+        self.neg_inf = -10e12
+
+    def forward(self, xm, xf):
+        # b,c,t,f
+        b,c,t,f = xf.shape
+        xm_1 = self.pointwise_conv1(xm)
+        xf_1= self.pointwise_conv2(xf)
+        padd = torch.zeros((b,self.h,self.max_delay-1, f))
+        k = torch.cat((padd, xf_1), dim=2)
+        b,c,t,f = k.shape
+        k_tmp = k.reshape(b*c, t,f)
+        k_1 =  k_tmp.unfold(1, self.max_delay, step=1)
+        k_2 = k_1.chunk(b,0)
+        k_2 = torch.stack(k_2,dim=0)
+        q = xm_1.unsqueeze(-2)
+        corr = torch.matmul( q,k_2).squeeze(-2)
+        corr_1 = self.conv2d(corr)[...,:-2,:].squeeze(1)
+
+        # mask for previous padd
+        min_t = min(corr_1.size(1), self.max_delay)
+        mask = torch.ones(min_t, self.max_delay)
+        mask = torch.tril(mask).flip(dims=[-1])
+        mask = mask.unsqueeze(0).repeat(corr.size(0), 1,1)
+        mask = torch.logical_not(mask)
+        corr_1[:,:self.max_delay][mask] = self.neg_inf
+        corr_2 = torch.softmax(corr_1, dim=-1)
+
+        # weighted sum
+        b,c,t,f = xf.shape
+        padd = torch.zeros(b,c,self.max_delay-1,f)
+        xf_2 = torch.cat((padd, xf),-2).transpose(3,2)
+        xf_2 = xf_2.unfold(-1, self.max_delay, step=1)
+        corr_ = corr_2.unsqueeze(1).unsqueeze(1)
+        out = (corr_ * xf_2).sum(-1).transpose(3,2)
+        return out
+
+
+
+
+
 class Residual(nn.Module):
     def __init__(self, in_channel, out_channel, kernel_size, stride, padding) -> None:
         super().__init__()
@@ -21,11 +71,12 @@ class Residual(nn.Module):
 
 
 class PixelConv(nn.Module):
-    def __init__(self, in_ch, up_scaler, kernel_size, stride, padding) -> None:
+    def __init__(self, in_ch, out_ch, up_scaler, kernel_size, stride, padding) -> None:
         super().__init__()
         self.up_scaler = up_scaler
+        self.out_ch = out_ch
         self.padding = [kernel_size[0] - stride[0], kernel_size[1] - stride[1]]
-        self.conv2d = nn.Conv2d(in_channels=in_ch, out_channels=in_ch*up_scaler, stride=stride, kernel_size=kernel_size, padding=padding)
+        self.conv2d = nn.Conv2d(in_channels=in_ch, out_channels=out_ch*up_scaler, stride=stride, kernel_size=kernel_size, padding=padding)
 
         # self.pixel = nn.Sequential(nn.Conv2d(in_channels=in_ch, out_channels=out_ch, stride=stride, kernel_size=kernel_size, padding=padding),
         #                            nn.PixelShuffle(2*in_ch))
@@ -37,20 +88,12 @@ class PixelConv(nn.Module):
         out1 = out1[...,:-self.padding[0],:-self.padding[1]]
         out2 = torch.chunk(out1, self.up_scaler, dim=1)
         out3 = torch.stack(out2, -1)
-        out4 = out3.reshape(b,c,t,-1)
+        out4 = out3.reshape(b,self.out_ch,t,-1)
 
         # out = self.pixel(x)
         return out4
 
 
-class AlignBlock(nn.Module):
-    def __init__(self) -> None:
-        super().__init__()
-        # self.pointConv = nn.Conv2d()
-
-    def forward(self, x, y):
-        pass
-        return 0
 
 
 class SkipBlock(nn.Module):
@@ -99,7 +142,7 @@ class CCMBlock(nn.Module):
         return 0
     
 class deepvqe(nn.Module):
-    def __init__(self, near_in_channel=[2, 64, 128], far_in_channel=[2, 32, 128], kernel_size=[4, 3], stride=[1, 2]) -> None:
+    def __init__(self, near_in_channel=[2, 64, 128],out_ch=27, far_in_channel=[2, 32, 128], kernel_size=[4, 3], stride=[1, 2]) -> None:
         super().__init__()
         self.rnn_infeat = int(np.ceil( 240 / 2**5) * near_in_channel[-1])
         self.padding = [kernel_size[0] - stride[0], kernel_size[1] - stride[1]]
@@ -113,7 +156,7 @@ class deepvqe(nn.Module):
                                      nn.ELU(),
                                      Residual(in_channel=near_in_channel[2], out_channel=near_in_channel[2], kernel_size=kernel_size, stride=(1, 1), padding=(kernel_size[0]-1,kernel_size[1]-1))
                                      )
-        self.conv2_3 = nn.Sequential(nn.Conv2d(in_channels=near_in_channel[2], out_channels=near_in_channel[2], kernel_size=kernel_size, stride=stride,padding=self.padding),
+        self.conv2_3 = nn.Sequential(nn.Conv2d(in_channels=near_in_channel[2]*2, out_channels=near_in_channel[2], kernel_size=kernel_size, stride=stride,padding=self.padding),
                                      nn.BatchNorm2d(near_in_channel[2]),
                                      nn.ELU(),
                                      Residual(in_channel=near_in_channel[2], out_channel=near_in_channel[2], kernel_size=kernel_size, stride=(1, 1), padding=(kernel_size[0]-1,kernel_size[1]-1))
@@ -143,29 +186,35 @@ class deepvqe(nn.Module):
         self.skip2 = SkipBlock(in_ch=near_in_channel[-1])
         self.skip1 = SkipBlock(in_ch=near_in_channel[1])
         self.deconv2_5 = nn.Sequential(Residual(in_channel=near_in_channel[-1], out_channel=near_in_channel[-1], kernel_size=kernel_size, stride=(1,1), padding=(kernel_size[0]-1,kernel_size[1]-1)),
-                                       PixelConv(in_ch=near_in_channel[-1], up_scaler=2, kernel_size=kernel_size, stride=(1,1), padding=(kernel_size[0]-1, kernel_size[1]-1)),
+                                       PixelConv(in_ch=near_in_channel[-1],out_ch=near_in_channel[-1], up_scaler=2, kernel_size=kernel_size, stride=(1,1), padding=(kernel_size[0]-1, kernel_size[1]-1)),
                                        nn.BatchNorm2d(near_in_channel[-1]),
                                        nn.ELU())
         self.deconv2_4 = nn.Sequential(Residual(in_channel=near_in_channel[-1], out_channel=near_in_channel[-1], kernel_size=kernel_size, stride=(1,1), padding=(kernel_size[0]-1,kernel_size[1]-1)),
-                                       PixelConv(in_ch=near_in_channel[-1], up_scaler=2, kernel_size=kernel_size, stride=(1,1), padding=(kernel_size[0]-1, kernel_size[1]-1)),
+                                       PixelConv(in_ch=near_in_channel[-1],out_ch=near_in_channel[-1], up_scaler=2, kernel_size=kernel_size, stride=(1,1), padding=(kernel_size[0]-1, kernel_size[1]-1)),
                                        nn.BatchNorm2d(near_in_channel[-1]),
                                        nn.ELU())        
         self.deconv2_3 = nn.Sequential(Residual(in_channel=near_in_channel[-1], out_channel=near_in_channel[-1], kernel_size=kernel_size, stride=(1,1), padding=(kernel_size[0]-1,kernel_size[1]-1)),
-                                       PixelConv(in_ch=near_in_channel[-1], up_scaler=2, kernel_size=kernel_size, stride=(1,1), padding=(kernel_size[0]-1, kernel_size[1]-1)),
+                                       PixelConv(in_ch=near_in_channel[-1],out_ch=near_in_channel[-1], up_scaler=2, kernel_size=kernel_size, stride=(1,1), padding=(kernel_size[0]-1, kernel_size[1]-1)),
                                        nn.BatchNorm2d(near_in_channel[-1]),
                                        nn.ELU())
-        self.deconv2_2 = nn.Sequential(Residual(in_channel=near_in_channel[-1], out_channel=near_in_channel[1], kernel_size=kernel_size, stride=(1,1), padding=(kernel_size[0]-1,kernel_size[1]-1)),
-                                       PixelConv(in_ch=near_in_channel[1], up_scaler=2, kernel_size=kernel_size, stride=(1,1), padding=(kernel_size[0]-1, kernel_size[1]-1)),
+        self.deconv2_2 = nn.Sequential(Residual(in_channel=near_in_channel[-1], out_channel=near_in_channel[-1], kernel_size=kernel_size, stride=(1,1), padding=(kernel_size[0]-1,kernel_size[1]-1)),
+                                       PixelConv(in_ch=near_in_channel[-1],out_ch=near_in_channel[1], up_scaler=2, kernel_size=kernel_size, stride=(1,1), padding=(kernel_size[0]-1, kernel_size[1]-1)),
                                        nn.BatchNorm2d(near_in_channel[1]),
                                        nn.ELU())       
-        self.deconv2_1 = nn.Sequential(Residual(in_channel=near_in_channel[1], out_channel=near_in_channel[0], kernel_size=kernel_size, stride=(1,1), padding=(kernel_size[0]-1,kernel_size[1]-1)),
-                                       PixelConv(in_ch=near_in_channel[0], up_scaler=2, kernel_size=kernel_size, stride=(1,1), padding=(kernel_size[0]-1, kernel_size[1]-1)))     
+        self.deconv2_1 = nn.Sequential(Residual(in_channel=near_in_channel[1], out_channel=near_in_channel[1], kernel_size=kernel_size, stride=(1,1), padding=(kernel_size[0]-1,kernel_size[1]-1)),
+                                       PixelConv(in_ch=near_in_channel[1],out_ch=out_ch, up_scaler=2, kernel_size=kernel_size, stride=(1,1), padding=(kernel_size[0]-1, kernel_size[1]-1)))     
         self.bottleneck = Bottleneck(infeat_num=self.rnn_infeat, rnn_hidden=self.rnn_infeat,rnn_num=1)
+        self.alignblock = AlignBlock(128, 32, 32)
 
     def forward(self, x, far=None):
         x1 = self.conv2_1(x)[...,:-self.padding[0],:]
         x2 = self.conv2_2(x1)[...,:-self.padding[0],:]
-        x3 = self.conv2_3(x2)[...,:-self.padding[0],:]
+
+        far_1 = self.far_conv2_1(far)[...,:-self.padding[0],:]
+        far_2 = self.far_conv2_2(far_1)[...,:-self.padding[0],:]
+        align_out = self.alignblock(x2,far_2)
+        x2_conc = torch.cat((x2, align_out), 1)
+        x3 = self.conv2_3(x2_conc)[...,:-self.padding[0],:]
         x4 = self.conv2_4(x3)[...,:-self.padding[0],:]
         x5 = self.conv2_5(x4)[...,:-self.padding[0],:]
         x6 = self.bottleneck(x5)
@@ -177,26 +226,31 @@ class deepvqe(nn.Module):
         skip3 = self.skip3(x3, x8)
         x9 = self.deconv2_3(skip3)
         skip2 = self.skip2(x2, x9)
-        x10 = self.deconv2_2(skip2) # todo:bug
+        x10 = self.deconv2_2(skip2) 
         skip1 = self.skip1(x1, x10)
         x11 = self.deconv2_1(skip1)
-        return x6
+        return x11
 
 
 if __name__ == "__main__":
+    # sig = torch.randn(2,128,3,60)
+    # sig1 = torch.randn(2,128,3,60)
+    # alignModule = AlignBlock(128)
+    # out = alignModule(sig, sig1)
 
-    sig = torch.randn(2,27,1,4)
-    ccm = CCMBlock()
-    out = ccm(sig)
+    # sig = torch.randn(2,27,1,4)
+    # ccm = CCMBlock()
+    # out = ccm(sig)
     sig = torch.randn(2, 2, 1, 240)
+    far = torch.randn(2,2,1,240)
     net = deepvqe()
-    out3 = net(sig)
+    out3 = net(sig,far)
     # conv = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=(4,3), stride=(1,2),padding=(3,1))
     # out2 = conv(sig)
     # residual = Residual(in_channel=3, out_channel=3, kernel_size=(4,3), stride=(1,1), padding=(3,2))
     # out1 = residual(sig)
     # sig1 = torch.randn(2,128,1,8)
-    # pixel = PixelConv(in_ch=128, up_scaler=2, kernel_size=(4,3), stride=(1,1), padding=(3,2))
+    # pixel = PixelConv(in_ch=128, out_ch=64,up_scaler=2, kernel_size=(4,3), stride=(1,1), padding=(3,2))
 
     # out = pixel(sig1)
     print('sc')
